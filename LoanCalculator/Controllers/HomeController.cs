a3975915e5e78c233355c68faedc8d1ed5d33af8
Kyle Givler <xvegan88x@gmail.com>
2021-07-25 15:54:05 -0400

    Work on the view and such

--- LoanCalculator/Controllers/HomeController.cs
+++ LoanCalculator/Controllers/HomeController.cs
@@ -28,6 +28,27 @@ namespace LoanCalculator.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult LoanCalculator()
+        {
+            Loan loan = new();
+
+            loan.Payment = 0.0m;
+            loan.TotalInterest = 0.0m;
+            loan.TotalCost = 0.0m;
+            loan.Rate = 3.5m;
+            loan.Amount = 15000m;
+            loan.Term = 60;
+
+            return View(loan);
+        }
+
+        [HttpPost]
+        public IActionResult LoanCalculator(Loan loan)
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
